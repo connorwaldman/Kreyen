970c1fb743dd9e9c05e72366b66af91cae26b73f
connorwaldman <waldmanconnor@gmail.com>
2020-04-10 15:49:57 -0400

    cleanup and add comments

--- quadseq.lua
+++ quadseq.lua
@@ -1,14 +1,20 @@
-unpack = table.unpack
+-- Input 1: clock 1
+-- Input 2: clock 2
+-- Output 1: sequence 1
+-- Output 2: gate 1
+-- Output 3: sequence 2
+-- Output 4: gate 2
 
 sequence1 = {}
 sequence2 = {}
 index1 = 0
 index2 = 0
+s = table.unpack
 
 -- Scales
-diatonic = {0,2,4,5,7,9,11,12,14,16,17,19,21,23}
-pent_major = {0,2,4,7,9,12,14,16,19,21}
-   
+diatonic = {0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23}
+pent_major = {0, 2, 4, 7, 9, 12, 14, 16, 19, 21}
+
 function init()
     input[1].mode("change", 1.0, 0.1, "rising")
     input[1].change = handleChangeClock1
@@ -21,7 +27,6 @@ function handleChangeClock1(s)
     output[1].volts = step / 12.0
     output[1].slew = 0.01
     index1 = ((index1 + 1) % #sequence1)
-    -- print(step)
 end
 
 function handleChangeClock2(s)
@@ -29,12 +34,14 @@ function handleChangeClock2(s)
     output[3].volts = step / 12.0
     output[3].slew = 0.01
     index2 = ((index2 + 1) % #sequence2)
-    -- print(step)
 end
 
--- user functions invoked from druid --
+--            USER FUNCTIONS             --
+--         (invoked from druid)          --
+-- seq1() will set values for sequence 1 --
+-- seq2() will set values for sequence 2 --
+-- To load a scale: seq1(s(scale))   --
 
--- To load scale: seq(unpack(scale))
 function seq1(...)
     sequence1 = {...}
     show(sequence1)
@@ -55,12 +62,13 @@ function show(sequence)
     print(seq)
 end
 
--- Test
-sequence1 = {0,7,4,7,9}
-sequence2 = {0,9,4,9}
--- seq1(0,1,2,3,4,5)
--- seq1(unpack(diatonic))
+--Initialize sequences:
+sequence1 = {11, 11, 9, 9, 12, 2, 14, 14, 16, 16, 11, 0, 14, 14}
+sequence2 = {19, 18, 16, 14, 11, 9, 7}
 
+-- TEST:
+-- seq1(0,1,2,3,4,5)
+-- seq1(s(diatonic))
 -- handleChangeClock1(1)
 -- handleChangeClock1(2)
 -- handleChangeClock1(3)
